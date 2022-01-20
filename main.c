648ef5f52fba5b1ec557c802105b5166f9c5ae0e
draialexis <drai.alexis@gmail.com>
2022-01-20 04:47:45 +0100

    Refactor neumann

--- main.c
+++ main.c
@@ -2,13 +2,20 @@
 
 #define LAPS 100
 
-int main() {
-
-    int n = 1234; // converges towards 0, reaches it at i=56
+void neumann(int n, int laps) {
     for (int i = 0; i < LAPS; i++) {
         printf("i=%d; %04d\n", i, n);
         n = ((n * n) / 100) % 10000;
     }
+}
+
+int main() {
+
+    printf("###########1###########\n");
+    neumann(1234, 100); // converges towards 0, reaches it at i=56
+
+    printf("###########2###########\n");
+    neumann(4100, 100); // is stuck in a short period at i%4==0 (4100, 8100, 6100, 2100, ...)
 
     return 0;
 }
