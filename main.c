6d0f30e600adc34bbbf67b1c24f9273675674349
draialexis <drai.alexis@gmail.com>
2022-01-20 04:48:52 +0100

    Fix typos

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #define LAPS 100
 
 void neumann(int n, int laps) {
-    for (int i = 0; i < LAPS; i++) {
+    for (int i = 0; i < laps; i++) {
         printf("i=%d; %04d\n", i, n);
         n = ((n * n) / 100) % 10000;
     }
@@ -12,10 +12,10 @@ void neumann(int n, int laps) {
 int main() {
 
     printf("###########1###########\n");
-    neumann(1234, 100); // converges towards 0, reaches it at i=56
+    neumann(1234, LAPS); // converges towards 0, reaches it at i=56
 
     printf("###########2###########\n");
-    neumann(4100, 100); // is stuck in a short period at i%4==0 (4100, 8100, 6100, 2100, ...)
+    neumann(4100, LAPS); // is stuck in a short period at i%4==0 (4100, 8100, 6100, 2100, ...)
 
     return 0;
 }
