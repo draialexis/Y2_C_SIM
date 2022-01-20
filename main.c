98cd39b0dba5293bf104cf994c6f0c56ffedabea
Alexis DRAI <drai.alexis@gmail.com>
2022-01-20 10:58:40 +0100

    Do exs 5 and 6, screw up 7, leave noisy classroom

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define LAPS 100
 
 void initArr(int *res, int n)
 {
@@ -24,8 +23,9 @@ int *mkArr(int n)
     return res;
 }
 
-void printArr(int *arr, int n)
+void printArr(char *msg, int *arr, int n)
 {
+    printf("%s: ", msg);
     for (int i = 0; i < n; i++)
     {
         printf("%d", arr[i]);
@@ -58,25 +58,65 @@ void diceThrow(int sides, int throws, int *res)
     }
 }
 
+void myLCG(int a, int c, int m, int *res, int n, int x_0)
+{
+    int x_prev;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            x_prev = x_0;
+        }
+        else
+        {
+            x_prev = res[i - 1];
+        }
+        res[i] = (x_prev * a + c) % m;
+    }
+}
+
+/*
+int intRand() {
+    return (_ * 5 + 1) % 16;
+}
+*/
+
+void myLCG_float(int a, int c, int m, float *res, int n, float x_0)
+{
+    float x_prev;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            x_prev = x_0;
+        }
+        else
+        {
+            x_prev = res[i - 1];
+        }
+        res[i] = (x_prev * a + c) % m;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
-
-    int *arr = mkArr(LAPS);
+    int laps = 100;
+    int *arr = mkArr(laps);
 
     printf("########### 1 ###########\n");
-    neumann(1234, LAPS, arr); // converges towards 0, reaches it at i=56
-    printArr(arr, LAPS);
+    neumann(1234, laps, arr); // converges towards 0, reaches it at i=56
+    printArr("seed 1234", arr, laps);
 
     printf("########### 2 ###########\n");
-    neumann(4100, LAPS, arr); // is stuck in a short period at i%4==0 (4100, 8100, 6100, 2100, ...)
-    printArr(arr, LAPS);
-    neumann(1324, LAPS, arr); // reaches 4100 at i=92, and then gets stuck in the above-mentioned period
-    printArr(arr, LAPS);
-    neumann(1301, LAPS, arr); // reaches 6100 at i=10, and then gets stuck in the above-mentioned period
-    printArr(arr, LAPS);
-    neumann(3141, LAPS, arr); // reaches 100 at i=7 and just stays there (_100_ * 100 / 100 % 10000 = _100_)
-    printArr(arr, LAPS);
+    neumann(4100, laps, arr); // is stuck in a short period at i%4==0 (4100, 8100, 6100, 2100, ...)
+    printArr("seed 4100", arr, laps);
+    neumann(1324, laps, arr); // reaches 4100 at i=92, and then gets stuck in the above-mentioned period
+    printArr("seed 1324", arr, laps);
+    neumann(1301, laps, arr); // reaches 6100 at i=10, and then gets stuck in the above-mentioned period
+    printArr("seed 1301", arr, laps);
+    neumann(3141, laps, arr); // reaches 100 at i=7 and just stays there (_100_ * 100 / 100 % 10000 = _100_)
+    printArr("seed 3141", arr, laps);
 
     printf("########### 3 ###########\n");
     /* 
@@ -87,15 +127,43 @@ int main()
      */
 
     printf("########### 4 ###########\n");
-    int *coinArr = mkArr(2);
+    int sides = 2;
+    int *coinArr = mkArr(sides);
     for (int i = 10; i <= 1000; i *= 10)
     {
-        diceThrow(2, i, coinArr);
-        printArr(coinArr, 2);
-        initArr(coinArr, 2);
+        diceThrow(sides, i, coinArr);
+        printArr("coin flips", coinArr, sides);
+        initArr(coinArr, sides);
     }
 
     // the results seem equidistributed -- no clear bias one way or another
+
+    printf("########### 5 ###########\n");
+    sides = 6;
+    int *d6Arr = mkArr(sides);
+    for (int i = 10; i <= 1000; i *= 10)
+    {
+        diceThrow(sides, i, d6Arr);
+        printArr("d6 throws", d6Arr, sides);
+        initArr(d6Arr, sides);
+    }
+
+    sides = 10;
+    int *d10Arr = mkArr(sides);
+    for (int i = 10; i <= 1000; i *= 10)
+    {
+        diceThrow(sides, i, d10Arr);
+        printArr("d10 throws", d10Arr, sides);
+        initArr(d10Arr, sides);
+    }
+    diceThrow(sides, 1000000, d10Arr);
+    printArr("d10 throws", d10Arr, sides);
+
+    printf("########### 6 ###########\n");
+    int n = 32;
+    arr = mkArr(n);
+    myLCG(5, 1, 16, arr, n, 5);
+    printArr("x_i+1 = (5 * x_i + 1) mod 16", arr, n);
 
     return 0;
 }
