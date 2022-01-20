4515dcd27ed33ce1509bf4f0066d2f50da6fa4e0
draialexis <drai.alexis@gmail.com>
2022-01-20 04:28:34 +0100

    Touch up ex1

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
+
 #define LAPS 100
 
 int main() {
 
-    int n = 1234; // converges towards 0, reaches it at i=54
+    int n = 1234; // converges towards 0, reaches it at i=55
     for (int i = 0; i < LAPS; i++) {
-        n = n * n;
-        //01522756
-        n = n / 100;
-        //015227
-        n = n % 10000;
-        //5227
+        n = ((n * n) / 100) % 10000;
         printf("i=%d; %04d\n", i, n);
     }
 
