1de3398ef6824d5fd2cc6ca99daea3559fe75db3
draialexis <drai.alexis@gmail.com>
2022-02-10 11:02:54 +0100

    Do tp3q1

--- TP3/main.c
+++ TP3/main.c
@@ -18,11 +18,27 @@
 ///////////////////////////////   BUSINESS    /////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
+double simPi(int inPoints)
+{
+    double m, x, y;
+    for (int i = 0; i < inPoints; i++)
+    {
+        x = genrand_real1();
+        y = genrand_real1();
+        if (pow(x, 2) + pow(y, 2) < 1)
+        {
+            m++;
+        }
+    }
+    return (m / inPoints) * 4;
+}
+
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////   TESTS       /////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
-int main(void) {
+int main(void)
+{
     // init by M.M.
     int i, j, k;
     unsigned long init[4] = {0x123, 0x234, 0x345, 0x456};
@@ -33,20 +49,26 @@ int main(void) {
     printf("########### 1 ###########\n");
 
     printf("1000 outputs of genrand_int32()\n");
-    for (i = 0; i < 1000; i++) {
+    for (i = 0; i < 1000; i++)
+    {
         printf("%10lu ", genrand_int32());
         if (i % 5 == 4) printf("\n");
     }
     printf("\n1000 outputs of genrand_real2()\n");
-    for (i = 0; i < 1000; i++) {
+    for (i = 0; i < 1000; i++)
+    {
         printf("%10.8f ", genrand_real2());
         if (i % 5 == 4) printf("\n");
     }
     // making sure that it checks out compared to ../matsumoto/mt19937ar.out
 
     printf("########### 2 ###########\n");
-
-
+    double res1;
+    for (i = 1000; i <= 1000000000; i *= 1000)
+    {
+        res1 = simPi(i);
+        printf("Pi approx, at i = %d: %10.8f\n", i, res1);
+    }
 
     return 0;
 }
